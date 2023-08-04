036651fa2724b6325666f84981a3982b95f86b9d
Anirudh <60171262+Blank333@users.noreply.github.com>
2023-08-05 02:59:03 +0530

    Added responsive code to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -26,12 +26,52 @@ function Navbar() {
               className='h-20'
             />
           </Link>
+        </div>
+        <div>
           <button
             onClick={toggleMenu}
-            className='p-2 text-gray-800 focus:outline-none lg:hidden'
+            className={`p-2 text-gray-800 focus:outline-none lg:hidden ${
+              isMenuOpen ? "translate-x-32" : ""
+            } transition-transform duration-300`}
           >
             <Menu />
           </button>
+
+          {isMenuOpen && (
+            <ul className='w-full bg-white py-2 shadow-lg flex flex-col items-center justify-center gap-2 lg:hidden'>
+              <li className='hover:underline'>
+                <Link to='#about'>Categories</Link>
+                <ExpandMore />
+              </li>
+              <li className='hover:underline'>
+                <Link href='#contact'>Deals</Link>
+              </li>
+              <li className='hover:underline'>
+                <Link href='#contact'>What's New</Link>
+              </li>
+              <li className='hover:underline'>
+                <Link href='#contact'>Delivery</Link>
+              </li>
+              <li className='hover:underline text-center'>
+                <input
+                  className='rounded-3xl w-5/6 px-4 py-2 bg-gray-100 placeholder:text-gray-500'
+                  type='search'
+                  placeholder='Search'
+                />
+                <Search className='absolute right-8 top-[9.6em] text-gray-500' />
+              </li>
+              <li className='hover:underline'>
+                <Link href=''>
+                  <PermIdentity className='relative bottom-[3px] right-1' />
+                  Account
+                </Link>
+              </li>
+              <li className='hover:underline'>
+                <BookmarksOutlined className='relative bottom-[3px] right-1' />
+                <Link href=''>Bookmarks</Link>
+              </li>
+            </ul>
+          )}
         </div>
 
         <ul className='lg:flex-row flex-col justify-between items-center h-full container m-auto lg:flex hidden'>
